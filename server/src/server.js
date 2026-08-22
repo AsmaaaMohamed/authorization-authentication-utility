558194868e48f66c94f50e42ad4063b60b0b63e1
Ahmed-Samir12 <ahmed.samir27901@gmail.com>
2026-08-22 08:59:29 +0300

    better files structure, seperated server.js and app.js and putted inside src folder

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+import connectDB from "./config/mongodb.js";
+import "dotenv/config";
+import app from "./app.js";
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception — shutting down");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+const port = process.env.PORT || 5000;
+connectDB();
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!, Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
